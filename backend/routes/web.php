ce9ba203457a4a87ae2fecefcea5ecd1f0ec7049
jergonzalezco <jergonzalezco@unal.edu.co>
2023-10-26 10:12:25 -0500

    Se creó el backend

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GuestsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/guests', [GuestsController::class, 'store'])->name('guests.store');
 });
